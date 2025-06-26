411691e3658fa3741c6b4ec08256b851f55f1560
rafael <rmarqdev@gmail.com>
2025-06-26 19:14:21 +0000

    REfactor products component

---
+++ src/app/ui/products.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import { mockProducts } from "../data/mockProducts";
+import Product from './product'
+export default function Products() {
+  return (
+    <div style={{textTransform:'uppercase', fontWeight:'300'}}>
+      <p>Products</p>
+      <Row>
+        {mockProducts.map((product: any) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </Row>
+    </div>
+  );
+}
