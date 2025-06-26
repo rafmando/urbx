fda9ce9193deae3496a767bcc8f97ba487efd3f2
rafael <rmarqdev@gmail.com>
2025-06-26 19:10:56 +0000

    Remove footer and header from page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Header from "./ui/header";
-import Footer from './ui/footer'
 import { Container } from "react-bootstrap";
+import Splash from "./ui/splash";
 
 export default function Page() {
   return (
     <div>
-      <Header/>
         <main className="py-3">
           <Container>
-            <h1>WElcome</h1>
+            <Splash/>
           </Container>
         </main>
-      <Footer/>
+    
     </div>
   );
 }
