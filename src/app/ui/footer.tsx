d662e956bf2a38db20fabe818e98f4bca7a795ad
rafael <rmarqdev@gmail.com>
2025-06-26 09:13:07 +0000

    Add footer

---
+++ src/app/ui/footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+export default function Footer() {
+  return (
+    <footer>
+        <Container>
+        <Row>
+        <Col className='text-center py-3'>Copyright &copy; UrbanX</Col>
+      </Row>
+        </Container>
+      
+    </footer>
+  )
+}
